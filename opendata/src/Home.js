0812c6b1696a303601878b8d9373416218015bb7
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2019-12-10 11:28:45 +0100

    Start

---
+++ opendata/src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css';
+
+
+export default function Home() {
+
+    return (
+        <div>
+            <header>
+                <div id="Hero">
+                    <h1 id="pagename">OPENDATA</h1>
+                    
+                </div>
+            </header>
+            <footer>
+                <p>Hello</p>
+            </footer>
+        </div>
+        
+    )}
