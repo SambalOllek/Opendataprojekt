a97d3a52ecdcb60bf77c91d2778b161aa1d56872
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2019-12-12 12:59:21 +0100

    Files moved, readme finished for review

--- ReadMe2.md
+++ ReadMe2.md
@@ -1,2 +1,10 @@
 # Opendata Projekt 
-This opendata project is constructed to be used as a third party car sales website that shows active sales in a selected region
+This opendata project is a react app constructed to be used as a third party car sales website that shows active sales in a selected region in Sweden. The application consists of one page where all information is accessible, after pressing the login button a modal with login input is featured.
+___
+
+## 1 | Getting Started 
+For this application to function properly you need to start up react in the terminal. You do this by right-clicking any file in the src folder and clicking "open terminal". When the terminal is active, you type npm start and press enter. This should launch a chrome page with the page in question displayed.
+____
+
+## 2 | Navigating the page
+When you first enter the page you are greeted by a login button, by logging into the page your preferred information for locations is stored and will be there the next time you log in. If you dont already have an account, there is also a button to register a new account. If you proceed to scroll down the page you will se a map and a search field, here you can search for places in sweden and the map will then display where cars in that area are sold, as well as some brief information about the cars when clicked.
--- src/Login.js
+++ src/Login.js
@@ -2,33 +2,34 @@ import React from "react";
 
 
 //När man klickar på knappen så öppnas en modal
-export function Login(props){
-    const[ShowModal, setShowModal] = React.useState(false);
+export function Login(props) {
+    const [ShowModal, setShowModal] = React.useState(false);
     let modal = "modal";
-    if(ShowModal === true){
+    if (ShowModal === true) {
         modal = "modal is-active";
     }
 
 
-    function openModal(){
+    function openModal() {
         setShowModal(true);
     }
 
-    function closeModal(){
+    function closeModal() {
         setShowModal(false);
     }
 
 
-return <div className= "Login">
-    <button id="Login" onClick={openModal}>Login</button>
+    return <div className="Login">
+        <button id="Login" onClick={openModal}>Login</button>
         <div className={modal}>
-            <div className="modal-background"></div>
-            <div className="modal-content">
-                <input className="input" id="usn" type="text" placeholder="Username"/>
-                <input className="input" id="psw" type="text" placeholder="Password"/>
-                <button type="button" id="ClickLogin">Login</button>
-            </div>
+            <div className="modal-background">
+                <div className="modal-content">
+                    <input className="input" id="usn" type="text" placeholder="Username" />
+                    <input className="input" id="psw" type="text" placeholder="Password" />
+                    <button type="button" id="ClickLogin">Login</button>
+                </div>
                 <button className="modal-close is-large" onClick={closeModal} aria-label="close">modal</button>
+            </div>
         </div>
     </div>
 }
