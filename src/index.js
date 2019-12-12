c508309bc06c9008cccfdbc68dc11f5a7b3723ce
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2019-12-12 11:05:20 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "../node_modules/bulma/css/bulma.css";
-//import App from './App';
-import "./Index.css";
 import Home from './Home';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
