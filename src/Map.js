7fb1066f42406dd4a8dbb2badd3c323b1dd46a89
lol3937 <43269056+lol3937@users.noreply.github.com>
2019-12-13 09:34:55 +0100

    Maps changes

--- src/Map.js
+++ src/Map.js
@@ -1,3 +1,19 @@
 //Kartan som används
 import Map from 'ol/Map';
-import React from 'react';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+
+var map = new Map({
+    view: new View({
+      center: [0, 0],
+      zoom: 1
+    }),
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    target: 'map'
+  });
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "../node_modules/bulma/css/bulma.css";
 import Home from './Home';
+import Map from './Map';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
