2e20da8b8aec68d7cac96dd4e39e5ae38819d7a5
erikh <erikh@DESKTOP-4MTUUBP>
2019-12-13 19:19:02 +0100

    Created Car class

---
+++ src/entities/Car.js
@@ -0,0 +1,19 @@
+
+export class Car{
+
+    constructor({id, link, address, year, fuel, gearbox, brand, model, price, mileage, drivewheel, regnum}){
+        this.id = id;
+        this.link = link;
+        this.address = address;
+        this.year = year;
+        this.fuel = fuel;
+        this.gearbox = gearbox;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.mileage = mileage;
+        this.drivewheel = drivewheel;
+        this.regnum = regnum;
+    }
+    
+}
