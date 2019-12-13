d2e8c7e967725207719b971a84362df25a435eb0
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2019-12-13 10:38:30 +0100

    buttons added

--- src/Home.js
+++ src/Home.js
@@ -18,12 +18,90 @@ export default function Home() {
             </header>
 
 
-            <footer>
-                <div id="container">
-                    <h1 id="Searchhead">Hitta bilar i ditt område</h1>
-                </div>
-            </footer>
-        </div >
 
-    )
+            <div id="container">
+                <h1 id="Searchhead">Hitta bilar i ditt område</h1>
+
+                <div class="field has-addons">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Sök efter en plats"/>
+                    </div>
+                        <div class="control">
+                            <a class="button is-info">
+                                Search
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="field has-addons">
+  <div class="control">
+    <div class="select is-halfwidth">
+      <select name="country">
+        <option value="Norrbotten">Norrbotten</option>
+        <option value="Västerbotten">Västerbotten</option>
+        <option value="Västernorrland">Västernorrland</option>
+        <option value="Jämtland">Jämtland</option>
+        <option value="Gävleborg">Gävleborg</option>
+        <option value="Dalarna">Dalarna</option>
+        <option value="Värmland">Värmland</option>
+        <option value="Örebro">Örebro</option>
+        <option value="Västmanland">Västmanland</option>
+        <option value="Uppsala">Uppsala</option>
+        <option value="Stockholm">Stockholm</option>
+        <option value="Södermanland">Södermanland</option>
+        <option value="Västra Götaland">Västra Götaland</option>
+        <option value="Östergötland">Östergötland</option>
+        <option value="Jönköping">Jönköping</option>
+        <option value="Kalmar">Kalmar</option>
+        <option value="Gotland">Gotland</option>
+        <option value="Kronoberg">Kronoberg</option>
+        <option value="Halland">Halland</option>
+        <option value="Skåne">Skåne</option>
+        <option value="Blekinge">Blekinge</option>
+      </select>
+    </div>
+  </div>
+  <div class="control">
+    <button type="submit" class="button is-primary">Stad</button>
+  </div>
+</div>
+                    <div class="field has-addons">
+  <div class="control">
+    <div class="select is-halfwidth">
+      <select name="country">
+        <option value="Norrbotten">Norrbotten</option>
+        <option value="Västerbotten">Västerbotten</option>
+        <option value="Västernorrland">Västernorrland</option>
+        <option value="Jämtland">Jämtland</option>
+        <option value="Gävleborg">Gävleborg</option>
+        <option value="Dalarna">Dalarna</option>
+        <option value="Värmland">Värmland</option>
+        <option value="Örebro">Örebro</option>
+        <option value="Västmanland">Västmanland</option>
+        <option value="Uppsala">Uppsala</option>
+        <option value="Stockholm">Stockholm</option>
+        <option value="Södermanland">Södermanland</option>
+        <option value="Västra Götaland">Västra Götaland</option>
+        <option value="Östergötland">Östergötland</option>
+        <option value="Jönköping">Jönköping</option>
+        <option value="Kalmar">Kalmar</option>
+        <option value="Gotland">Gotland</option>
+        <option value="Kronoberg">Kronoberg</option>
+        <option value="Halland">Halland</option>
+        <option value="Skåne">Skåne</option>
+        <option value="Blekinge">Blekinge</option>
+      </select>
+    </div>
+  </div>
+  <div class="control">
+    <button type="submit" class="button is-primary">Län</button>
+  </div>
+</div>
+
+                </div>
+                <footer>
+                </footer>
+            </div >
+
+            )
 }
