b7b504a8266a347559a4cc8d80a2725c1fa4924b
Olle Pettersson <43269121+SambalOllek@users.noreply.github.com>
2019-12-17 14:47:50 +0100

    fix

--- src/Map.js
+++ src/Map.js
@@ -1,57 +1,70 @@
 //Kartan som används
-/*import Map from 'ol/Map';
+//import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';*/
+import OSM from 'ol/source/OSM';
+import Feature from 'ol/Feature';
+import markerIcon from './sass/Addons/markerIcon.png';
 
 
 
 import {
-    interaction, layer, custom, control, //name spaces
-    Interactions, Overlays, Controls,     //group
-    Map, Layers, Overlay, Util,    //objects
-    geocoder
-  } from "react-openlayers";
-  import React from 'react';
-  import "./sass/Map.scss";
-  import { GeoCode } from "../geo-coder-master/src/providers/geo-code.js";
+  interaction, layer, custom, control, //name spaces
+  Interactions, Overlays, Controls,     //group
+  Map, Layers, Overlay, Util,    //objects
+  geocoder
+} from "react-openlayers";
+import React from 'react';
+import "./sass/Map.scss";
 import Point from "ol/geom/Point";
 import Style from "ol/style/Style";
 
-export default function Maps(props){
- 
+export default function Maps(props) {
 
-    return <div id="map">
-  <Map view={{center: [59.334591, 18.063240], zoom: 4}}>
-  <Layers>
-    <layer.Tile/>
-  </Layers>
-  <Overlays>
-  </Overlays>
-  <Controls attribution={false} zoom={true}>
-    <control.Rotate />
-    <control.Zoom />
-  </Controls>
 
-</Map>
-</div>
+  return <div id="map">
+    <Map view={{ center: [59.334591, 18.063240], zoom: 4 }}>
+      <Layers>
+        <layer.Tile />
+      </Layers>
+      <Overlays>
+      </Overlays>
+      <Controls attribution={false} zoom={true}>
+        <control.Rotate />
+        <control.Zoom />
+      </Controls>
+
+    </Map>
+  </div>
 }
 
 //<interaction.Select style={selectedMarkerStyle} />
-const coordinatesArray = []
-;
-
-const geoMarkerArray = coordinatesArray.map(coordinates => {
-  return new Feature({
-    type: "geoMarker",
-    geometry: new Point(ol.proj.fromLonLat([coordinates.longitude,coordinates.latitude]))
-  })
-});
-
-let styles = {
-  "geoMarker": new Style({
-    image: new Icon({
-      src: markerIcon
-    })
-  })
-};
+//const coordinatesArray = [
+//  {
+//    longitude: 59.334591,
+//    latitude: 18.063240
+//  },
+//  {
+//    longitude: 59.334591,
+//    latitude: 18.063240
+//  },
+//  {
+//    longitude: 59.334591,
+//    latitude: 18.063240
+//  }]
+//  ;
+//
+//const geoMarkerArray = coordinatesArray.map(coordinates => {
+//  return new Feature({
+//    type: "geoMarker",
+//    geometry: new Point(ol.proj.fromLonLat([coordinates.longitude, coordinates.latitude]))
+//  })
+//});
+//
+//let styles = {
+//  "geoMarker": new Style({
+//    image: new Icon({
+//      src: markerIcon
+//    })
+//  })
+//};
