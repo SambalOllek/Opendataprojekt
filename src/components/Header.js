4994b7a2eb43d0876ad0231fb78ccad4c5f5907e
Erik Hult <32297312+zyxago@users.noreply.github.com>
2020-01-15 15:18:32 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react"
 import Login from "./Login";
 
-export default function Header(props) {
+export default function Header({setIsLoggedIn}) {
     return (
         <header>
             <div id="Hero">
                 <div>
                 </div>
-                <Login/>
+                <Login setIsLoggedInt={setIsLoggedIn}/>
                 <h2 id="maintext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat sapien eu
                     libero
                     vestibulum dignissim. Ut lacinia aliquam fermentum. Nunc metus nunc</h2>
