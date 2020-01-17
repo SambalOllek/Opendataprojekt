053a1dedd1ca7d8d46b8cb6bba6f4380635f3152
Erik Hult <32297312+zyxago@users.noreply.github.com>
2020-01-17 14:02:14 +0100

    Update CarInfo.js

--- src/components/CarInfo.js
+++ src/components/CarInfo.js
@@ -1,9 +1,8 @@
 import React from "react"
 import "../sass/CarInfo.scss";
 
-export default function CarInfo({feature}){
-    const car = feature.values_.car
-    console.log(feature.values_.car);
+export default function CarInfo({car }){
+    console.log(car);
     return (
         <div id="CarInfo">
             <p>Bil Info:</p>
@@ -14,6 +13,7 @@ export default function CarInfo({feature}){
             <p>{car.gearbox}</p>
             <p>{car.mileage}</p>
             <p>{car.price}</p>
+            <input type="button" className="button" value="Spara i intresse listan"/>
         </div>
     );
 }
